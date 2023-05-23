66d79a69c1a0b6b5ed985b608f5184d033fc9199
ruperto1020 <126834536+ruperto1020@users.noreply.github.com>
2023-05-23 20:09:47 -0300

    Add files via upload

---
+++ ruperto-main/js/Boat.js
@@ -0,0 +1,25 @@
+class Boat{
+    constructor(x,y,w,h,boatPos){
+        var opitions={isStatic:false}
+
+        this.w=w;
+        this.h=h;
+        this.boatPos=boatPos
+        this.image=loadImage("./assets/boat.png")
+        this.body=Bodies.rectangle(x,y,w,h,opitions)
+        World.add(world,this.body)
+    }
+
+
+    display(){
+    var pos=this.body.position
+    var angle=this.body.angle
+
+    push()
+    translate(pos.x,pos.y)
+    rotate(angle)
+    imageMode(CENTER)
+    image(this.image,0,this.boatPos,this.w,this.h)
+    pop()
+    }
+}
